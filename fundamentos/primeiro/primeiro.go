ab32920e332b8a2391ea69bc36dc530879170a07
olvneto <olvneto@yahoo.com.br>
2020-08-04 23:12:53 -0300

    Primeiro dia

---
+++ fundamentos/primeiro/primeiro.go
@@ -0,0 +1,12 @@
+//Programas executáveis iniciam pelo pacote main
+package main
+
+/*Import da linguagem
+ */
+import "fmt"
+
+//Porta de entrada para um progrma em go
+func main() {
+	fmt.Print("Primeiro ")
+	fmt.Print("Programa")
+}
